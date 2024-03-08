c7b7af6afa15d7c42f6cd1978ced5d88b60b9036
vdema <demaisip.v@northeastern.edu>
2024-03-08 15:27:12 -0500

    Formatted pfp and basic info

--- app/Pages/ViewProfilePage/page.tsx
+++ app/Pages/ViewProfilePage/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 const ViewProfilePage = () => {
   const [profileData, setProfileData] = useState<Profile>();
-  const userId = 3;
+  const userId = 225;
 
   useEffect(() => {
     const fetchProfileInfo = async () => {
@@ -20,7 +20,7 @@ const ViewProfilePage = () => {
         if (error) {
           throw error;
         }
-        setProfileData(data);
+        setProfileData(data[0]);
       } catch (error) {
         console.log("Error fetching profile info:", error);
       }
@@ -30,17 +30,25 @@ const ViewProfilePage = () => {
   }, []);
 
   return (
-    <div>
-      <p>Username: {profileData?.username}</p>
-      {profileData?.image && (
-        <Image
-          src={profileData?.image}
-          alt="Profile.pic"
-          height="520"
-          width="480"
-          priority={false}
-        />
-      )}
+    <div className="h-screen w-screen">
+      <div className="w-1/5 m-5">
+        {profileData?.image && (
+          <Image className="rounded-full"
+            src={profileData?.image}
+            //src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+            alt="Profile.pic"
+            height="300"
+            width="350"
+            priority={false}
+          />
+        )}
+      </div>
+      <div className="w-1/5 justify-center items-center text-center m-5">
+        <h1 className="text-4xl">{profileData?.username}</h1>
+      </div>
+      <div className="bg-black w-1/5 h-4/6 m-5">
+          <h1>FEET</h1>
+      </div>
     </div>
   );
 };
--- app/components/PhotoDisplay.tsx
+++ app/components/PhotoDisplay.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import Image from "next/image";
 import { Profile } from "../Types/types";
 
-const PhotoDisplay = ({ profile }) => {
+const PhotoDisplay = ({ profile } : { profile: Profile }) => {
   return (
     <div>
       <div className="flex items-center border-4 rounded shadow-lg w-48 h-52">
         <div className="overflow-hidden rounded w-full h-full">
-          <Image
+          { profile.image ? <Image
             alt="Profile pic"
             src={profile.image}
             height="520"
             width="460"
             objectFit="cover"
             className="rounded"
-          />
+          /> : null }
         </div>
       </div>
     </div>
