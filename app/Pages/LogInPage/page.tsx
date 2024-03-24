1da8a506743626fb8b554f6a3fd845627d783f00
Juwang110 <Juwang110@gmail.com>
2024-03-24 13:12:59 -0400

    Update page.tsx

--- app/Pages/LogInPage/page.tsx
+++ app/Pages/LogInPage/page.tsx
@@ -6,7 +6,9 @@ import {
   Typography,
   TextField,
   Container,
-  Paper,
+  Card,
+  CardHeader,
+  CardContent,
 } from "@mui/material";
 import { Profile } from "@/app/Types/types";
 import { supabase } from "@/app/utils/supabase";
@@ -76,87 +78,167 @@ const LogInPage = () => {
   return (
     <Container
       sx={{
-        my: 10,
-        width: { xs: 200, sm: 250, md: 500, lg: 600 },
-        height: "75vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        padding: "20px",
       }}
     >
-      <Paper
-        elevation={24}
+      <Card
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          width: 1,
-          height: 1,
           borderRadius: "25px",
           textAlign: "center",
+          padding: "20px",
         }}
       >
-        <Typography fontSize={40} sx={{ mt: 10 }}>
-          Welcome to Ludis!
-        </Typography>
-        <Stack spacing={2} sx={{ mt: 8, mx: 5 }}>
-          <TextField
-            id="outlined"
-            label="Username"
-            defaultValue=""
-            onChange={(e) => setUsername(e.target.value)}
-            sx={{
-              "& .MuiOutlinedInput-root": {
-                borderRadius: "20px",
-              },
-            }}
-          />
-          <TextField
-            id="outlined"
-            label="Email"
-            defaultValue=""
-            onChange={(e) => setEmail(e.target.value)}
-            sx={{
-              "& .MuiOutlinedInput-root": {
-                borderRadius: "20px",
-              },
-            }}
-          />
-          <TextField
-            id="outlined"
-            label="Password"
-            defaultValue=""
-            type="password"
-            error={errorText !== ""}
-            helperText={errorText}
-            onChange={(e) => {
-              const passwordValue = e.target.value;
-              setPassword(passwordValue);
-              if (passwordValue.length > 0 && passwordValue.length < 6) {
-                setErrorText("Password must be at least 6 characters long.");
-              } else {
-                setErrorText(""); // Clear error when user types
-              }
-            }}
-            sx={{
-              "& .MuiOutlinedInput-root": {
-                borderRadius: "20px",
-              },
-            }}
-          />
-        </Stack>
-        <Stack spacing={2} sx={{ mt: 7, mx: 3 }}>
-          <CustomButton
-            buttonProps={{
-              label: "Log In",
-              onClick: handleLogIn,
-            }}
-          />
-          <CustomButton
-            buttonProps={{
-              label: "Sign Up",
-              onClick: async () => await handleSignUp(),
-            }}
-          />
-        </Stack>
-      </Paper>
+        <CardHeader
+          titleTypographyProps={{ variant: "h3" }}
+          title="Welcome To Ludis!"
+          subheaderTypographyProps={{ variant: "subtitle1" }}
+          subheader="The place to find your gym partner"
+        />
+        <CardContent>
+          <Stack spacing={5}>
+            <Stack spacing={2}>
+              <TextField
+                id="outlined"
+                label="Username"
+                defaultValue=""
+                onChange={(e) => setUsername(e.target.value)}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    borderRadius: "20px",
+                  },
+                }}
+              />
+              <TextField
+                id="outlined"
+                label="Email"
+                defaultValue=""
+                onChange={(e) => setEmail(e.target.value)}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    borderRadius: "20px",
+                  },
+                }}
+              />
+              <TextField
+                id="outlined"
+                label="Password"
+                defaultValue=""
+                type="password"
+                error={errorText !== ""}
+                helperText={errorText}
+                onChange={(e) => {
+                  const passwordValue = e.target.value;
+                  setPassword(passwordValue);
+                  if (passwordValue.length > 0 && passwordValue.length < 6) {
+                    setErrorText(
+                      "Password must be at least 6 characters long."
+                    );
+                  } else {
+                    setErrorText(""); // Clear error when user types
+                  }
+                }}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    borderRadius: "20px",
+                  },
+                }}
+              />
+            </Stack>
+            <Stack spacing={2}>
+              <CustomButton
+                buttonProps={{
+                  label: "Log In",
+                  onClick: handleLogIn,
+                }}
+              />
+              <CustomButton
+                buttonProps={{
+                  label: "Sign Up",
+                  onClick: async () => await handleSignUp(),
+                }}
+              />
+            </Stack>
+          </Stack>
+        </CardContent>
+      </Card>
     </Container>
+    // <Container
+    //   sx={{
+    //     display: "flex",
+    //     alignItems: "center",
+    //     justifyContent: "center",
+    //     height: "100vh",
+    //   }}
+    // >
+    //   <Paper
+    //     elevation={24}
+    //     sx={{
+    //       display: "flex",
+    //       flexDirection: "column",
+    //       width: 0.5,
+    //       height: 700,
+    //       borderRadius: "25px",
+    //       textAlign: "center",
+    //     }}
+    //   >
+    //     <Typography fontSize={40} sx={{ mt: 10 }}>
+    //       Welcome to Ludis!
+    //     </Typography>
+    //     <Stack spacing={2} sx={{ mt: 8, mx: 5 }}>
+    //       <TextField
+    //         id="outlined"
+    //         label="Username"
+    //         defaultValue=""
+    //         onChange={(e) => setUsername(e.target.value)}
+    //         sx={{
+    //           "& .MuiOutlinedInput-root": {
+    //             borderRadius: "20px",
+    //           },
+    //         }}
+    //       />
+    //       <TextField
+    //         id="outlined"
+    //         label="Email"
+    //         defaultValue=""
+    //         onChange={(e) => setEmail(e.target.value)}
+    //         sx={{
+    //           "& .MuiOutlinedInput-root": {
+    //             borderRadius: "20px",
+    //           },
+    //         }}
+    //       />
+    //       <TextField
+    //         id="outlined"
+    //         label="Password"
+    //         defaultValue=""
+    //         onChange={(e) => setPassword(e.target.value)}
+    //         sx={{
+    //           "& .MuiOutlinedInput-root": {
+    //             borderRadius: "20px",
+    //           },
+    //         }}
+    //       />
+    //     </Stack>
+    //     <Stack spacing={2} sx={{ mt: 7, mx: 3 }}>
+    //       <CustomButton
+    //         buttonProps={{
+    //           label: "Log In",
+    //           onClick: handleLogIn,
+    //         }}
+    //       />
+    //       <CustomButton
+    //         buttonProps={{
+    //           label: "Sign Up",
+    //           onClick: async () => await handleSignUp(),
+    //         }}
+    //       />
+    //     </Stack>
+    //   </Paper>
+    // </Container>
   );
 };
 
